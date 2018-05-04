ebd70a7837aeb3dd604e7a2d5e1b7b1f3160192c
Jeff Li <jeff.li.cf@gmail.com>
2018-05-04 19:49:11 -0400

    Circle Arena - reset still bugged

---
+++ Dumb Racer/Assets/_Scripts/BoudaryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoudaryController : MonoBehaviour {
+
+    public Transform Spawn1;
+    public GameObject Player1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player1")
+        {
+            Instantiate(Player1, Spawn1.position, Spawn1.rotation);
+        }
+    }
+}
---
+++ Dumb Racer/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+    public Transform Spawn1;
+    private Transform myTrans;
+
+    // object orginal position
+    Vector3 orginalPos;
+    //object position
+    Vector3 myPos;
+    //object rotation
+    Vector3 myRot;
+    //object rotation 
+    float angle;
+
+    // Use this for initialization
+    void Start()
+    {
+        myTrans = transform;
+        myPos = myTrans.position;
+        myRot = myTrans.rotation.eulerAngles;
+    }
+
+    void Update()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //converting the object euler angle's magnitude from to Radians    
+        angle = myTrans.eulerAngles.magnitude * Mathf.Deg2Rad;
+
+
+
+        //rotate object Right & Left
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myRot.z -= rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myRot.z += rotationSpeed;
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            myPos.x += (Mathf.Cos(angle) * speed) * Time.deltaTime;
+            myPos.y += (Mathf.Sin(angle) * speed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            myPos.x += Mathf.Cos(angle) * Time.deltaTime;
+            myPos.y += Mathf.Sin(angle) * Time.deltaTime;
+        }
+
+        //Apply
+        myTrans.position = myPos;
+        myTrans.rotation = Quaternion.Euler(myRot);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "OB")
+        {
+            Reset();
+        }
+    }
+
+    void Reset() {
+        this.gameObject.transform.position = Spawn1.transform.position;
+    }
+}
